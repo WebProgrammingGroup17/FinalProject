c27e612fbcc01f73bc84b7f0c4c80cea133a283c
PhanThiHuynhNhu <23130224@st.hcmuaf.edu.vn>
2025-12-14 14:10:51 +0700

    Add product module with dao, servlet, db

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,61 @@
 package model;
 
 public class Product {
+    private int id;
+    private String name;
+    private int category;
+    private int brand;
+    private int price;
+    private String image;
+
+    public Product() {}
+    public Product(int id, String name, int category, int brand, int price, String image) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.brand = brand;
+        this.price = price;
+        this.image = image;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getCategory() {
+        return category;
+    }
+    public void setCategory(int category) {
+        this.category = category;
+    }
+    public int getBrand() {
+        return brand;
+    }
+    public void setBrand(int brand) {
+        this.brand = brand;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+//    @Override
+//    public String toString() {
+//
+//    }
 }
